f40e21dee3f3942c901298f45337c6d6a74e583d
Galyminsky-PC <jobforandoid@protonmail.com>
2022-05-26 12:01:15 +0600

    Добавлена и настроена разметка для recipe_list_item

--- app/src/main/java/com/jobforandroid/recipeforeveryday/MainActivity.kt
+++ app/src/main/java/com/jobforandroid/recipeforeveryday/MainActivity.kt
@@ -3,10 +3,13 @@ package com.jobforandroid.recipeforeveryday
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jobforandroid.recipeforeveryday.R
+import com.jobforandroid.recipeforeveryday.fragments.RecipeFragment
+import com.jobforandroid.recipeforeveryday.utils.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FragmentManager.setFragment(RecipeFragment.newInstance(),this)
     }
 }
